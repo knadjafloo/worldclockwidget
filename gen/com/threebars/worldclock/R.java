de49f89940bf5510d26cc4c380275557d9185951
knadjafloo <knadjafloo@Gmail.com>
2012-06-11 23:46:56 -0700

    some updates

--- gen/com/threebars/worldclock/R.java
+++ gen/com/threebars/worldclock/R.java
@@ -9,21 +9,180 @@ package com.threebars.worldclock;
 
 public final class R {
     public static final class attr {
+        /** <p>Must be a color value, in the form of "<code>#<i>rgb</i></code>", "<code>#<i>argb</i></code>",
+"<code>#<i>rrggbb</i></code>", or "<code>#<i>aarrggbb</i></code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+         */
+        public static final int dragndrop_background=0x7f010003;
+        /** <p>Must be a dimension value, which is a floating point number appended with a unit such as "<code>14.5sp</code>".
+Available units are: px (pixels), dp (density-independent pixels), sp (scaled pixels based on preferred font size),
+in (inches), mm (millimeters).
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+         */
+        public static final int expanded_height=0x7f010001;
+        /** <p>Must be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
+or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>".
+         */
+        public static final int grabber=0x7f010002;
+        /** <p>Must be a dimension value, which is a floating point number appended with a unit such as "<code>14.5sp</code>".
+Available units are: px (pixels), dp (density-independent pixels), sp (scaled pixels based on preferred font size),
+in (inches), mm (millimeters).
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+         */
+        public static final int normal_height=0x7f010000;
+        /** <p>Must be one of the following constant values.</p>
+<table>
+<colgroup align="left" />
+<colgroup align="left" />
+<colgroup align="left" />
+<tr><th>Constant</th><th>Value</th><th>Description</th></tr>
+<tr><td><code>none</code></td><td>-1</td><td></td></tr>
+<tr><td><code>fling</code></td><td>0</td><td></td></tr>
+<tr><td><code>slide</code></td><td>1</td><td></td></tr>
+<tr><td><code>slideRight</code></td><td>1</td><td></td></tr>
+<tr><td><code>slideLeft</code></td><td>2</td><td></td></tr>
+</table>
+         */
+        public static final int remove_mode=0x7f010004;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int grabber=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int okSearch=0x7f050001;
-        public static final int searchText=0x7f050000;
+        public static final int fling=0x7f040001;
+        public static final int icon=0x7f040007;
+        public static final int label=0x7f040008;
+        public static final int none=0x7f040000;
+        public static final int okSearch=0x7f040006;
+        public static final int searchText=0x7f040005;
+        public static final int slide=0x7f040002;
+        public static final int slideLeft=0x7f040004;
+        public static final int slideRight=0x7f040003;
     }
     public static final class layout {
         public static final int clock_appwidget_provider_info=0x7f030000;
         public static final int main=0x7f030001;
-        public static final int one_clock=0x7f030002;
+        public static final int main_list=0x7f030002;
+        public static final int one_clock=0x7f030003;
+        public static final int row=0x7f030004;
+        public static final int row2=0x7f030005;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
+        public static final int app_name=0x7f050001;
+        public static final int hello=0x7f050000;
     }
+    public static final class styleable {
+        /** Attributes that can be used with a TouchListView.
+           <p>Includes the following attributes:</p>
+           <table>
+           <colgroup align="left" />
+           <colgroup align="left" />
+           <tr><th>Attribute</th><th>Description</th></tr>
+           <tr><td><code>{@link #TouchListView_dragndrop_background com.threebars.worldclock:dragndrop_background}</code></td><td></td></tr>
+           <tr><td><code>{@link #TouchListView_expanded_height com.threebars.worldclock:expanded_height}</code></td><td></td></tr>
+           <tr><td><code>{@link #TouchListView_grabber com.threebars.worldclock:grabber}</code></td><td></td></tr>
+           <tr><td><code>{@link #TouchListView_normal_height com.threebars.worldclock:normal_height}</code></td><td></td></tr>
+           <tr><td><code>{@link #TouchListView_remove_mode com.threebars.worldclock:remove_mode}</code></td><td></td></tr>
+           </table>
+           @see #TouchListView_dragndrop_background
+           @see #TouchListView_expanded_height
+           @see #TouchListView_grabber
+           @see #TouchListView_normal_height
+           @see #TouchListView_remove_mode
+         */
+        public static final int[] TouchListView = {
+            0x7f010000, 0x7f010001, 0x7f010002, 0x7f010003,
+            0x7f010004
+        };
+        /**
+          <p>This symbol is the offset where the {@link com.threebars.worldclock.R.attr#dragndrop_background}
+          attribute's value can be found in the {@link #TouchListView} array.
+
+
+          <p>Must be a color value, in the form of "<code>#<i>rgb</i></code>", "<code>#<i>argb</i></code>",
+"<code>#<i>rrggbb</i></code>", or "<code>#<i>aarrggbb</i></code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+          @attr name android:dragndrop_background
+        */
+        public static final int TouchListView_dragndrop_background = 3;
+        /**
+          <p>This symbol is the offset where the {@link com.threebars.worldclock.R.attr#expanded_height}
+          attribute's value can be found in the {@link #TouchListView} array.
+
+
+          <p>Must be a dimension value, which is a floating point number appended with a unit such as "<code>14.5sp</code>".
+Available units are: px (pixels), dp (density-independent pixels), sp (scaled pixels based on preferred font size),
+in (inches), mm (millimeters).
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+          @attr name android:expanded_height
+        */
+        public static final int TouchListView_expanded_height = 1;
+        /**
+          <p>This symbol is the offset where the {@link com.threebars.worldclock.R.attr#grabber}
+          attribute's value can be found in the {@link #TouchListView} array.
+
+
+          <p>Must be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
+or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>".
+          @attr name android:grabber
+        */
+        public static final int TouchListView_grabber = 2;
+        /**
+          <p>This symbol is the offset where the {@link com.threebars.worldclock.R.attr#normal_height}
+          attribute's value can be found in the {@link #TouchListView} array.
+
+
+          <p>Must be a dimension value, which is a floating point number appended with a unit such as "<code>14.5sp</code>".
+Available units are: px (pixels), dp (density-independent pixels), sp (scaled pixels based on preferred font size),
+in (inches), mm (millimeters).
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+          @attr name android:normal_height
+        */
+        public static final int TouchListView_normal_height = 0;
+        /**
+          <p>This symbol is the offset where the {@link com.threebars.worldclock.R.attr#remove_mode}
+          attribute's value can be found in the {@link #TouchListView} array.
+
+
+          <p>Must be one of the following constant values.</p>
+<table>
+<colgroup align="left" />
+<colgroup align="left" />
+<colgroup align="left" />
+<tr><th>Constant</th><th>Value</th><th>Description</th></tr>
+<tr><td><code>none</code></td><td>-1</td><td></td></tr>
+<tr><td><code>fling</code></td><td>0</td><td></td></tr>
+<tr><td><code>slide</code></td><td>1</td><td></td></tr>
+<tr><td><code>slideRight</code></td><td>1</td><td></td></tr>
+<tr><td><code>slideLeft</code></td><td>2</td><td></td></tr>
+</table>
+          @attr name android:remove_mode
+        */
+        public static final int TouchListView_remove_mode = 4;
+    };
 }
--- src/com/threebars/worldclock/WorldClockWidgetActivity.java
+++ src/com/threebars/worldclock/WorldClockWidgetActivity.java
@@ -1,23 +1,59 @@
 package com.threebars.worldclock;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+import com.commonsware.cwac.tlv.TouchListView;
+
 import android.app.Activity;
+import android.app.ListActivity;
+import android.graphics.Typeface;
 import android.location.Address;
 import android.location.Geocoder;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DigitalClock;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class WorldClockWidgetActivity extends Activity {
 	
+	private static String[] items={"lorem", "ipsum", "dolor", "sit", "amet",
+		"consectetuer", "adipiscing", "elit", "morbi", "vel",
+		"ligula", "vitae", "arcu", "aliquet", "mollis",
+		"etiam", "vel", "erat", "placerat", "ante",
+		"porttitor", "sodales", "pellentesque", "augue", "purus"};
+	
+	private IconicAdapter adapter=null;
+	private ArrayList<String> array=new ArrayList<String>(Arrays.asList(items));
 	private static final String TAG = "WorldClockWidgetActivity";
 	
 	TextView searchTextView;
+	
+	private TouchListView.DropListener onDrop = new TouchListView.DropListener() {
+		@Override
+		public void drop(int from, int to) {
+			String item = adapter.getItem(from);
+
+			adapter.remove(item);
+			adapter.insert(item, to);
+		}
+	};
+
+	private TouchListView.RemoveListener onRemove = new TouchListView.RemoveListener() {
+		@Override
+		public void remove(int which) {
+			adapter.remove(adapter.getItem(which));
+		}
+	};
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -36,8 +72,24 @@ public class WorldClockWidgetActivity extends Activity {
 			}
 		});
         
-        TimeZoneParser parser = new TimeZoneParser(this);
-        parser.parseFile();
+//        TimeZoneParser parser = new TimeZoneParser(this);
+//        parser.parseFile();
+        
+        CustomDigitalClock digitalClock = new CustomDigitalClock(this);
+        digitalClock.setTextSize(45f);
+//        Typeface tf = Typeface.createFromAsset(getAssets(),
+//                "fonts/Y2K.ttf");
+//        digitalClock.setTypeface(tf);
+        setContentView(digitalClock);
+    	
+//    	setContentView(R.layout.main_list);
+//
+//		TouchListView tlv=(TouchListView)getListView();
+//		adapter=new IconicAdapter();
+//		setListAdapter(adapter);
+//		
+//		tlv.setDropListener(onDrop);
+//		tlv.setRemoveListener(onRemove);
     }
     
     public boolean getLatitudeAndLongitudeFromGoogleMapForAddress(String searchedAddress)
@@ -64,5 +116,28 @@ public class WorldClockWidgetActivity extends Activity {
 	        Log.d(TAG, "MY_ERROR : ############Address Not Found");
 	        return false;
 	    }
+	}
+    
+	class IconicAdapter extends ArrayAdapter<String> {
+		IconicAdapter() {
+			super(WorldClockWidgetActivity.this, R.layout.row2, array);
+		}
+		
+		public View getView(int position, View convertView,
+												ViewGroup parent) {
+			View row=convertView;
+			
+			if (row==null) {													
+				LayoutInflater inflater=getLayoutInflater();
+				
+				row=inflater.inflate(R.layout.row2, parent, false);
+			}
+			
+			TextView label=(TextView)row.findViewById(R.id.label);
+			
+			label.setText(array.get(position));
+			
+			return(row);
+		}
 	}
 }
